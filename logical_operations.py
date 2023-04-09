09689f0edbe5a7d73732299321722bdbe4807332
Evgeniy Kostromin <ewg.kostromin@yandexru>
2023-04-09 18:00:03 +0500

    Лекция 1

---
+++ logical_operations.py
@@ -0,0 +1,15 @@
+# Сравнение логические операции
+a = 1 > 3
+print(a)  # false
+b = 1 < 3 and 5 > 2
+print(b)  # true
+c = 1 == 2
+print(c)  # false
+d = 1 != 2
+print(d)  # true
+e = 'asd'
+f = 'asd'
+print(e == f)  # true
+# В Python можно использовать тройные и даже четверные неравенства:
+a = 1 < 3 < 5 < 10
+print(a)  # True
